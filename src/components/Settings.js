b64d1d4ee57271054456a2154fdc12cef5939d39
Sandbagger <wneal_19@yahoo.co.uk>
2019-01-01 21:20:32 +0000

    Re-organizing project structure to adopt a modular approach. Work in process.

---
+++ src/components/Settings.js
@@ -0,0 +1,21 @@
+import React from 'React';
+
+function Settings(props){
+    return (
+      <fieldset>
+        <legend>Game settings</legend>
+        <div>
+          <input type="radio" id="PvP" name="setting" onChange={props.onClickPvp} defaultChecked></input>
+          <label>P vs P</label>
+       </div>
+       <div>
+         <input type="radio" id="PvC" name="setting" onChange={props.onClickPvc} ></input>
+         <label>P vs C</label>
+       </div>
+       <button onClick={props.onClickReset}>Reset game</button>
+  </fieldset>
+      );
+  }
+  
+  
+  export default Setting;
