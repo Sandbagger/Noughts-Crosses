d6c3b35225a6adbbf649679f31b9c12bcdb4a13b
Sandbagger <wneal_19@yahoo.co.uk>
2019-01-07 07:11:15 +0000

    Added test for returnIndexOfAvailableMoves

---
+++ src/utils/miniMaxHelperFunctions.test.js
@@ -0,0 +1,30 @@
+import {returnIndexOfAvailableMoves} from './miniMaxHelperFunctions';
+
+describe('test', () => {
+    
+    var state1 = {
+        squares: [null, null, null, null, null, null, null, null, null],
+        xIsNext: true,
+        isPvp: true,
+        tick: 0,
+        points: 0
+    };
+
+    var state2 = {
+        squares: ['x', null, null, null, null, null, null, null, null],
+        xIsNext: true,
+        isPvp: true,
+        tick: 1,
+        points: 0
+    };
+
+
+it('if null replace with array index', ()=> {
+    expect(returnIndexOfAvailableMoves(state1)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8])
+})
+
+it('there should be no 0 in array', ()=> {
+    expect(returnIndexOfAvailableMoves(state2)).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
+})
+
+})
