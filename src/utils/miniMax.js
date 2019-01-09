b8bbc2d43cd52eec2ed9f71d1d85dd1a37d948ed
Sandbagger <wneal_19@yahoo.co.uk>
2019-01-09 07:09:51 +0000

    Refactored minimax function

--- src/utils/miniMax.js
+++ src/utils/miniMax.js
@@ -8,32 +8,27 @@ import {
   isGameWon,
   returnIndexOfAvailableMoves } 
   from "../utils/miniMaxHelperFunctions";
+import {calculateWinner} from "../utils/utilityFunctions"
 
 function minimax(state){
     state.tick = state.tick || 0;
     state.points = state.points || 0;
-  
-    if (isGameWon(state) || isDraw(state)){ 
+    if (calculateWinner(state.squares) || isDraw(state)){ 
       return score(state); 
     }
   
   let moves = returnIndexOfAvailableMoves(state);
   
-    var scores = moves.map(index => {
-    let move = simulateMove(clone(state), index);
-      return minimax(move)
-    })
-
+  var scores = moves.map(move => minimax(simulateMove(clone(state), move)))
+    
   let flattenScore = [].concat(...scores);
   
   let bestScore = returnBestScore(state, flattenScore);
-  
   
   if (state.tick === 0){
     return returnBestMove(state, flattenScore, moves);
   } else{return bestScore}
   
   }
-
 
   export default minimax;
